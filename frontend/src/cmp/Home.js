7f875681bbc0f1a73fe591bdb2b355c4bee4aa42
Ben Niesmertelny <ben@res1-184-90-dhcp.int.colorado.edu>
2022-03-06 07:45:17 -0700

    touched up some somponents

--- frontend/src/cmp/Home.js
+++ frontend/src/cmp/Home.js
@@ -1,4 +1,4 @@
-import { Typography, Box, Paper } from "@mui/material";
+import { Typography } from "@mui/material";
 // import { useState } from 'react';
 import Login from "./Login";
 
--- frontend/src/cmp/Navbar.js
+++ frontend/src/cmp/Navbar.js
@@ -1,36 +1,16 @@
-import { Paper, Box, Typography } from '@mui/material';
+import { Link, Box, Typography } from '@mui/material';
 import '../css/navbar.css';
 
-const NavLink = (props) => {
-
-    return (
-        <li>
-            <a 
-                className="nav-link"
-                href={ props.href }
-            >
-                <Typography variant="h6">
-                    { props.text }
-                </Typography>
-            </a>
-        </li>
-    );
-}
 
 const Navbar = () => {
 
     return (
-        <Paper className="nav-paper" elevation={5}>
-            <div className="nav-box">
-                <ul className="left links">
-                    <NavLink href="/" text="home" />
-                    <NavLink href="/about" text="about" /> 
-                </ul>
-                <ul className="right links">
-                    
-                </ul>
-            </div>
-        </Paper>
+        <Box className="nav-bar">
+            <Typography variant="h6" color="inherit" className="nav-links">
+                <Link  href="/" variant="inherit" color="inherit" underline="hover">home</Link>
+                <Link  href="/about" variant="inherit" color="inherit" underline="hover">about</Link>
+            </Typography>
+        </Box> 
     );
 }
 
