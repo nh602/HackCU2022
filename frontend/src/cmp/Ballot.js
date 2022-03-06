39feb567976422c753c644ce37adc2dbe1b729cd
Ben Niesmertelny <ben@res1-184-90-dhcp.int.colorado.edu>
2022-03-06 07:42:32 -0700

    ballot form submits data

--- frontend/src/cmp/Ballot.js
+++ frontend/src/cmp/Ballot.js
@@ -1,9 +1,50 @@
-import { Typography, RadioGroup, Radio, TextField, Button, Paper } from '@mui/material';
+import { Typography, Button, Paper } from '@mui/material';
+import { useState } from 'react';
+import UserService from '../services/UserService';
 import "../css/ballot.css";
 
 
+const mockBallot = {
+    "measures": [
+        {
+            "id": 0,
+            "measureText": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", 
+            "measureOptions": ["option A", "option B", "option C"], 
+            "writein": true
+        },
+        {
+            "id": 1,
+            "measureText": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", 
+            "measureOptions": ["option A", "option B", "option C", "option D"], 
+            "writein": false
+        },
+        {
+            "id": 2,
+            "measureText": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", 
+            "measureOptions": ["option A", "option B"], 
+            "writein": true
+        },
+        {
+            "id": 3,
+            "measureText": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", 
+            "measureOptions": ["option A", "option B", "option C"], 
+            "writein": false
+        }
+    ]
+}
 
 const Ballot = (props) => {
+
+    const [ballotData, setBallotData] = useState({})
+    const ballotInfo = mockBallot;
+
+    console.table(ballotInfo);
+
+    const handleChange = event => {
+        let newBallotData = ballotData;
+        newBallotData[event.target.name] = event.target.value;
+        setBallotData(newBallotData);
+    }
 
     const BallotMeasure = (props) => {
         let keyVar = 0;
@@ -14,6 +55,8 @@ const Ballot = (props) => {
                             type="radio" 
                             name={ `measure-${props.id}-option` }
                             id={ `measure-${props.id}-option-${keyVar}`}
+                            value={ optionText }
+                            onChange={ handleChange }
                         />
                     <Typography variant="subtitle2">
                         { optionText }
@@ -26,7 +69,7 @@ const Ballot = (props) => {
     
         return (
             <label className="measure-label">
-                <Typography variant="subtitle1" className="measure-text">{ props.measureText }</Typography>
+                <Typography variant="subtitle1" className="measure-text">{props.id+1}. { props.measureText }</Typography>
                 <div className="options">
                     { options }
                 </div>
@@ -38,7 +81,7 @@ const Ballot = (props) => {
     }
 
     // TODO change props.measures to some array from the api
-    const measures = props.measures.map((measureObj) => (
+    const measures = ballotInfo.measures.map((measureObj) => (
         <BallotMeasure 
             id={ measureObj.id }
             measureOptions={ measureObj.measureOptions }
@@ -48,10 +91,17 @@ const Ballot = (props) => {
         />
     ));
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(e)
-    }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.table(ballotData);
+        UserService.postBallot(ballotData)
+            .then((res) => {
+                window.open('/', "_self")
+            })
+            .catch((err) => {
+                console.log(err);
+            }, []);
+    };
 
     return (
         <div className="ballot page">
@@ -64,8 +114,9 @@ const Ballot = (props) => {
                     <Button 
                         variant="contained" 
                         type="submit" 
+                        color="secondary"
                     >
-                        Cast
+                       <strong>Cast ballot</strong>
                     </Button>
                 </form>
             </Paper>
