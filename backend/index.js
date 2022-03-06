003b3873e4bfdf8280a4ef78641aafec48cd40ab
nh602 <nhenley602@gmail.com>
2022-03-06 10:32:01 -0700

    cors fix

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  credentials: true,
+  preflightContinue: true,
+  origin: "http://localhost:3005",
+}));
 
 app.use('/api', router);
 
