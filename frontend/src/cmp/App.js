95702251ed61ee8b8fa5c75da27e9b5719cd17a3
Ben Niesmertelny <ben@cu-biot-15-10.203.187.40.int.colorado.edu>
2022-03-05 20:57:57 -0700

    ballot obj in app

--- frontend/src/cmp/App.js
+++ frontend/src/cmp/App.js
@@ -6,6 +6,35 @@ import Navbar from './Navbar.js';
 import Ballot from './Ballot.js';
 
 
+const ballotObj = {
+  "ballotMeasures": [
+      {
+          "id": 0,
+          "measureText": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", 
+          "measureOptions": ["option A", "option B", "option C"], 
+          "writein": true
+      },
+      {
+          "id": 1,
+          "measureText": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", 
+          "measureOptions": ["option A", "option B", "option C", "option D"], 
+          "writein": false
+      },
+      {
+          "id": 2,
+          "measureText": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", 
+          "measureOptions": ["option A", "option B"], 
+          "writein": true
+      },
+      {
+          "id": 3,
+          "measureText": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", 
+          "measureOptions": ["option A", "option B", "option C"], 
+          "writein": false
+      }
+  ]
+}
+
 function App() {
   return (
     
@@ -15,7 +44,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/ballot" element={<Ballot />} />
+          <Route path="/ballot" element={<Ballot measures={ ballotObj.ballotMeasures } state="CO"/>} />
         </Routes>
       </Router>
     </div>
