9c4f002fb5cb9414429e775b5e9950d1cdfcd643
Ben Niesmertelny <ben@cu-biot-15-10.203.187.40.int.colorado.edu>
2022-03-05 20:37:08 -0700

    ballot page

--- frontend/src/cmp/Ballot.js
+++ frontend/src/cmp/Ballot.js
@@ -1,23 +1,74 @@
-import { Typography } from '@mui/material';
+import { Typography, RadioGroup, Radio, TextField, Button } from '@mui/material';
+import "../css/ballot.css";
+
 
 const BallotMeasure = (props) =>{
 
-    const options = props.options.map()
+    let keyVar = 0;
+    const options = props.measureOptions.map(optionText => {
+        const option = (
+            <label className="option-label" key={ keyVar }>
+                    <input 
+                        type="radio" 
+                        name={ `measure-${props.id}-option` }
+                    />
+                <Typography variant="subtitle2">
+                    { optionText }
+                </Typography>
+            </label>
+        );
+        keyVar++;
+        return option;
+    });
+    if (props.writein) {
+        options.push(
+            <label className="option-label" key={ keyVar }>
+                    <input 
+                        type="radio" 
+                        name={ `measure-${props.id}-option` }
+                    />
+                    <TextField variant="outlined" label="write-in" />
+            </label>
+        );
+    }
 
     return (
-        <></>
+        <label className="measure-label">
+            <Typography variant="subtitle1" className="measure-text">{ props.measureText }</Typography>
+            <div className="options">
+                { options }
+            </div>
+        </label>
     );
+        
 }
 
 const Ballot = (props) => {
 
-    const measures = props.measures;
+    // TODO change props.measures to some array from the api
+    const measures = props.measures.map((measureObj) => (
+        <BallotMeasure 
+            id={ measureObj.id }
+            measureOptions={ measureObj.measureOptions }
+            measureText={ measureObj.measureText }
+            writein={ measureObj.writein }
+            key={ measureObj.id }
+        />
+    ));
 
     return (
         <div className="ballot page">
-            <Typography variant="h2" className="page-title">Ballot Page</Typography>
-            <form className='ballot-form' action="#" id="ballot-form">
-
+            <form className="ballot-form">
+                <Typography variant="h4" className="ballot-title">
+                    { props.state } State Ballot
+                </Typography>
+                { measures }
+                <Button 
+                    variant="contained" 
+                    type="submit" 
+                >
+                    Cast
+                </Button>
             </form>
         </div>
     );
