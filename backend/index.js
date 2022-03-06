2841fc9142c55d45638bf62afc6204f2837f89c3
nh602 <nhenley602@gmail.com>
2022-03-06 09:11:58 -0700

    corss

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,7 @@ require('dotenv').config();
 const db = require('./db');
 const router = require('./Routers/routes');
 const cookieParser = require('cookie-parser');
-
-const {Block, Blockchain} = require('./blockchain');
+const cors = require('cors');
 
 db.loadData();
 
@@ -12,22 +11,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors());
 
 app.use('/api', router);
-
-/*
-let bc = new Blockchain();
-let b = new Block();
-b.data = "test";
-bc.addBlock(b);
-b = new Block();
-b.data = "test2";
-bc.addBlock(b);
-
-for(block of bc.chain){
-  console.log(block);
-}
-*/
 
 let port = 3000;
 app.listen(port);
